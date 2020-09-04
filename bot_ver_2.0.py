f0e42c9588cf872a18b1bb1acfb4edd054ee4caf
Betonz <70288478+Betonz@users.noreply.github.com>
2020-09-04 17:02:28 +0300

    Update bot_ver_2.0.py
    Bot has been rewritten for the TG lib

--- bot_ver_2.0.py
+++ bot_ver_2.0.py
@@ -8,51 +8,48 @@ bot = telebot.TeleBot('1228594799:AAEekqyZCwbJ3wGrVjeDa64RDHiWgzjZm50')
 xls = pd.read_excel(r'https://github.com/Betonz/work_bot/blob/master/ListG.xlsx?raw=true', index_col=0)
 xls2 = pd.read_excel(r'https://github.com/Betonz/work_bot/blob/master/ListG.xlsx?raw=true', index_col=0, sheet_name='Listtwo')
  
-print (xls)
-print (xls2)
- 
-def room(a=()):
-    #a = int(a)
+def room(a=()):    
     if a in xls.index:
-        print ('Площадь {} помещения:'.format(a), xls.at[a, 'Plosh'])
-        print ('Высота потолков: ', xls.at[a, 'Plosh'])
+        bot.send_message(message.from_user.id, 'Площадь {} помещения:'.format(a), xls.at[a, 'Plosh'])
+        bot.send_message(message.from_user.id, 'Высота потолков: ', xls.at[a, 'Plosh'])
         c = xls.at[a, 'Arenda']
         if c == True:
-            print ('Договор аренды: ', xls.at[a, 'Dogovor'])
+            bot.send_message(message.from_user.id, 'Договор аренды: ', xls.at[a, 'Dogovor'])
         else:
-            print ('Арендаторы отсутствуют')
+            bot.send_message(message.from_user.id, 'Арендаторы отсутствуют')
     else:
-        print ('Нет такого помещения')
+        bot.send_message(message.from_user.id, 'Нет такого помещения')
         
- 
+
 def floor(a=int()):
     x = int(a.replace(" этаж", ""))
     if x in xls2.index:
-        print ('Площадь {} этажа: '.format(x), xls2.at[x,'Plosh2'])
-        print ('Кабинетов на этаже: ', xls2.at[x,'Cabs'])
-        print ('Занятых арендаторами площадей: ', xls2.at[x, 'Arenda S'])
-        print ('Свободных площадей: ', xls2.at[x, 'Free S'])
+        bot.send_message(message.from_user.id, 'Площадь {} этажа: '.format(x), xls2.at[x,'Plosh2'])
+        bot.send_message(message.from_user.id, 'Кабинетов на этаже: ', xls2.at[x,'Cabs'])
+        bot.send_message(message.from_user.id, 'Занятых арендаторами площадей: ', xls2.at[x, 'Arenda S'])
+        bot.send_message(message.from_user.id, 'Свободных площадей: ', xls2.at[x, 'Free S'])
     else:
-        print ('Нет такого этажа')
+        bot.send_message(message.from_user.id, 'Нет такого этажа')
         
  
 def helpus():
-    print ('Поиск по помещениям: введите номер в формате "А101-1" или "А101"')
-    print ('Поиск по этажу: введите этаж в формате "1 этаж"')
+    bot.send_message(message.from_user.id, 'Поиск по помещениям: введите номер в формате "А101-1" или "А101"')
+    bot.send_message(message.from_user.id, 'Поиск по этажу: введите этаж в формате "1 этаж"')
     
     
-# Добавить в цикл: если в а будет слово Этаж, запусти floor, и т.д. Б121-1
- 
-while True:    
-    incom_value = input('Введите значение : ')
-    floor_t = bool(re.search(r'^\d{1}\sэтаж', incom_value))
-    help_t = bool(re.search(r'^\помощь', incom_value))      
-    room_t = bool(re.search(r'^(А|Б|В|Г){1}\d{3}\W{1}\d{1}|^(А|Б|В|Г){1}\d{3}', incom_value))
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    floor_t = bool(re.search(r'^\d{1}\sэтаж', message))
+    help_t = bool(re.search(r'^\помощь', message))      
+    room_t = bool(re.search(r'^(А|Б|В|Г){1}\d{3}\W{1}\d{1}|^(А|Б|В|Г){1}\d{3}', message))
     if floor_t == True:
-        floor (incom_value)
+        floor (message)
     elif help_t == True:
-        helpus()
+        helpus ()
     elif room_t == True:
-        room (incom_value)
-    else: 
-        print ('Значение не верно')
+        room_t (message)
+    else:
+        bot.send_message(message)
+
+bot.polling(none_stop=True, interval=0)
+
